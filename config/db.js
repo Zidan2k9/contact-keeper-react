d5cfaf540bb80fc94b5e31e1171f9d79d8cad699
ZainLaptop <zain.ul_abdeen1191@live.ie>
2019-06-25 01:02:42 +0100

    Converted connectDB method to an async await function with a try catch block

--- config/db.js
+++ config/db.js
@@ -1,18 +1,19 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 
-const connectDB = () => {
-  mongoose
-    .connect(process.env.MONGO_URI, {
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useCreateIndex: true,
       useFindAndModify: false
-    })
-    .then(() => console.log("MongoDB Connected"))
-    .catch(err => {
-      console.log(err.message);
-      process.exit(1);
     });
+
+    console.log("MongoDB Connected");
+  } catch (err) {
+    console.log(err.message);
+    process.exit(1);
+  }
 };
 
 module.exports = connectDB;
