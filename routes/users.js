3585523085c666279058e6b0408250f2d249c9c6
ZainLaptop <zain.ul_abdeen1191@live.ie>
2019-06-27 01:26:14 +0100

    Each user has a jwt token associated with their login

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+//require("dotenv").config();
 const { check, validationResult } = require("express-validator");
+const config = require("config");
 
 const User = require("../models/User");
 
@@ -47,7 +50,23 @@ router.post(
 
       await user.save();
 
-      res.send("User saved");
+      const payload = {
+        user: {
+          id: user.id
+        }
+      };
+
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        {
+          expiresIn: 360000
+        },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
     } catch (err) {
       console.error(err.message);
       res.status(500).send("Server Error");
