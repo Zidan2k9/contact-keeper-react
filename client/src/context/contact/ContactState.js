52a3a89aceec0dd6965ab4717fdbd15e873244fd
ZainLaptop <zain.ul_abdeen1191@live.ie>
2019-06-29 02:28:30 +0100

    Created Contacts and ContactItem components, Contact details and edit/delete buttons are displayed on a contact item for each contact

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -33,7 +33,7 @@ const ContactState = props => {
         name: "Harry White",
         email: "harry@gmail.com",
         phone: "333-333-3333",
-        type: "personal"
+        type: "professional"
       }
     ]
   };
