a9f64a1ead3f0df9464c53cf87fcd69a78b30046
ZainLaptop <zain.ul_abdeen1191@live.ie>
2019-07-02 17:04:58 +0100

    A users contact can edit and update an existing contact

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -65,6 +65,30 @@ const ContactState = props => {
     }
   };
 
+  //Update contact
+  const updateContact = async contact => {
+    const config = {
+      headers: {
+        "Content-Type": "application/json"
+      }
+    };
+
+    try {
+      const res = await axios.put(
+        `/api/contacts/${contact._id}`,
+        contact,
+        config
+      );
+
+      dispatch({ type: UPDATE_CONTACT, payload: res.data });
+    } catch (error) {
+      dispatch({
+        type: CONTACT_ERROR,
+        payload: error.response.msg
+      });
+    }
+  };
+
   //Clear Contacts
   const clearContacts = () => {
     dispatch({ type: CLEAR_CONTACTS });
@@ -78,11 +102,6 @@ const ContactState = props => {
   //Clear current contact
   const clearCurrent = () => {
     dispatch({ type: CLEAR_CURRENT });
-  };
-
-  //Update contact
-  const updateContact = contact => {
-    dispatch({ type: UPDATE_CONTACT, payload: contact });
   };
 
   //Filter Contacts
