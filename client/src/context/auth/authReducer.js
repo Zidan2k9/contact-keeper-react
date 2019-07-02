b621a0f38f6853bd4591a89435d06392400d80cc
ZainLaptop <zain.ul_abdeen1191@live.ie>
2019-07-02 02:36:42 +0100

    Users registered remained logged in because of the authentication of their jwt token with the API

--- client/src/context/auth/authReducer.js
+++ client/src/context/auth/authReducer.js
@@ -6,11 +6,19 @@ import {
   LOGIN_SUCCESS,
   LOGIN_FAIL,
   LOGOUT,
-  CLEAR_ERRORS
+  CLEAR_ERRORS,
+  AUTH_ERROR
 } from "../types";
 
 export default (state, action) => {
   switch (action.type) {
+    case USER_LOADED:
+      return {
+        ...state,
+        isAuthenticated: true,
+        loading: false,
+        user: action.payload
+      };
     case REGISTER_SUCCESS:
       localStorage.setItem("token", action.payload.token);
       return {
@@ -20,6 +28,7 @@ export default (state, action) => {
         loading: false
       };
     case REGISTER_FAIL:
+    case AUTH_ERROR:
       localStorage.removeItem("token");
       return {
         ...state,
